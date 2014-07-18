f26c2807dc7e64a910cfffbd7193fb5a230085ab
srai <srai@yahoo-inc.com>
2014-07-17 17:07:34 -0700

    Initial project files with Parse Pod

---
+++ StudentCompanion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StudentCompanion
+//
+//  Created by Satyajit Rai on 7/17/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
